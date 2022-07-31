9697ee448797580d65644486b18c4b150f19bede
Anupam <anupamk@iitbhilai.ac.in>
2022-07-31 15:34:12 +0530

    main file to run and get the results in json

---
+++ Twitter_scrapper/main_twitter.py
@@ -0,0 +1,18 @@
+import tweets
+import time
+
+tweets.books_Stationaries()
+time.sleep(20*60)
+tweets.electronics()
+time.sleep(20*60)
+tweets.home_Funiture()
+time.sleep(20*60)
+tweets.sports_More()
+time.sleep(20*60)
+tweets.beauTy()
+time.sleep(20*60)
+tweets.kids_Babies()                                    
+time.sleep(20*60)
+tweets.Fashion()
+print("Execution Completed sucessfully!!")
+
