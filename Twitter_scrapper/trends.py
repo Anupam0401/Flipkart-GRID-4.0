50cfe0010740537d1e1c0f56492165f8d8eb2f15
Anupam <anupamk@iitbhilai.ac.in>
2022-07-31 15:33:54 +0530

    scrapping done for the trending section form twitter form the keywords

---
+++ Twitter_scrapper/trends.py
@@ -0,0 +1,12 @@
+import time
+import tweets as tw 
+from datetime import datetime, timedelta
+# # run script every 10 minutes past 5 hours 
+
+while 1:
+    tw.top_trends()
+    dt = datetime.now() + timedelta(hours=2)
+    dt = dt.replace(minute=10)
+
+    while datetime.now() < dt:
+        time.sleep(1)
